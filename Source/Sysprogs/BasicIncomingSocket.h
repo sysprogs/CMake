d2d3f1c965b11ef84fdeaf568dbb2ea2e7352c7b
Sysprogs <sysprogs@sysprogs.com>
2018-10-09 19:52:19 -0700

    Fixed break-in request handling

--- Source/Sysprogs/BasicIncomingSocket.h
+++ Source/Sysprogs/BasicIncomingSocket.h
@@ -1,18 +1,14 @@
 #include "cmSystemTools.h"
 
 #ifdef _WIN32
-#  include <WinSock2.h>
+#include <WinSock2.h>
 typedef int socklen_t;
 #else
-#  include <sys/socket.h>
-#  include <netinet/ip.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 typedef int SOCKET;
-static void closesocket(SOCKET socket)
-{
-	close(socket);
-}
+static void closesocket(SOCKET socket) { close(socket); }
 #endif
-
 
 /*
 	This is a very basic abstraction of the UNIX socket with blocking all-or-nothing I/O.
@@ -21,86 +17,91 @@ static void closesocket(SOCKET socket)
 class BasicIncomingSocket
 {
 private:
-  SOCKET m_Socket, m_AcceptedSocket;
+	SOCKET m_Socket, m_AcceptedSocket;
 
 public:
-  BasicIncomingSocket(int tcpPort)
-    : m_Socket(0)
-    , m_AcceptedSocket(0)
-  {
-    sockaddr_in addr = {
-      AF_INET,
-    };
-    addr.sin_port = htons(tcpPort);
+	BasicIncomingSocket(int tcpPort) : m_Socket(0), m_AcceptedSocket(0)
+	{
+		sockaddr_in addr = {
+			AF_INET,
+		};
+		addr.sin_port = htons(tcpPort);
 
-    m_Socket = socket(AF_INET, SOCK_STREAM, 0);
+		m_Socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (m_Socket < 0) {
-      cmSystemTools::Error(
-        "Failed to create a listening socket for the debug server.");
-      return;
-    }
+		if (m_Socket < 0)
+		{
+			cmSystemTools::Error("Failed to create a listening socket for the debug server.");
+			return;
+		}
 
-    if (bind(m_Socket, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-      closesocket(m_Socket);
-      m_Socket = -1;
-      cmSystemTools::Error(
-        "Failed to bind the listening socket for the debug server.");
-      return;
-    }
+		if (bind(m_Socket, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+		{
+			closesocket(m_Socket);
+			m_Socket = -1;
+			cmSystemTools::Error("Failed to bind the listening socket for the debug server.");
+			return;
+		}
 
-    if (listen(m_Socket, 1) < 0) {
-      closesocket(m_Socket);
-      m_Socket = -1;
-      cmSystemTools::Error(
-        "Failed to start the listening socket for the debug server.");
-      return;
-    }
-  }
+		if (listen(m_Socket, 1) < 0)
+		{
+			closesocket(m_Socket);
+			m_Socket = -1;
+			cmSystemTools::Error("Failed to start the listening socket for the debug server.");
+			return;
+		}
+	}
 
-  bool Accept()
-  {
-    if (m_Socket < 0)
-      return false;
-    sockaddr_in addr;
-    socklen_t len = sizeof(addr);
-    m_AcceptedSocket = accept(m_Socket, (struct sockaddr*)&addr, &len);
-    if (m_AcceptedSocket < 0) {
-      cmSystemTools::Error("Failed to accept a debugging connection.");
-      return false;
-    }
+	bool Accept()
+	{
+		if (m_Socket < 0)
+			return false;
+		sockaddr_in addr;
+		socklen_t len = sizeof(addr);
+		m_AcceptedSocket = accept(m_Socket, (struct sockaddr *)&addr, &len);
+		if (m_AcceptedSocket < 0)
+		{
+			cmSystemTools::Error("Failed to accept a debugging connection.");
+			return false;
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  bool HasIncomingData()
-  {
-    char tmp;
-    int result = recv(m_Socket, &tmp, 1, MSG_PEEK);
-    return result > 0;
-  }
+	bool HasIncomingData()
+	{
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(m_AcceptedSocket, &fds);
 
-  bool Write(const void* pData, size_t size)
-  {
-    if (m_AcceptedSocket <= 0)
-      return false;
-    int done = send(m_AcceptedSocket, (const char*)pData, size, 0);
-    return done == size;
-  }
+		timeval timeout = {
+			0,
+		};
+		int result = select(m_AcceptedSocket + 1, &fds, NULL, NULL, &timeout);
+		return result > 0;
+	}
 
-  bool ReadAll(void* pData, size_t size)
-  {
-    if (m_AcceptedSocket <= 0)
-      return false;
-    int done = recv(m_AcceptedSocket, (char*)pData, size, MSG_WAITALL);
-    return done == size;
-  }
+	bool Write(const void *pData, size_t size)
+	{
+		if (m_AcceptedSocket <= 0)
+			return false;
+		int done = send(m_AcceptedSocket, (const char *)pData, size, 0);
+		return done == size;
+	}
 
-  ~BasicIncomingSocket()
-  {
-    if (m_AcceptedSocket > 0)
-      closesocket(m_AcceptedSocket);
-    if (m_Socket > 0)
-      closesocket(m_Socket);
-  }
+	bool ReadAll(void *pData, size_t size)
+	{
+		if (m_AcceptedSocket <= 0)
+			return false;
+		int done = recv(m_AcceptedSocket, (char *)pData, size, MSG_WAITALL);
+		return done == size;
+	}
+
+	~BasicIncomingSocket()
+	{
+		if (m_AcceptedSocket > 0)
+			closesocket(m_AcceptedSocket);
+		if (m_Socket > 0)
+			closesocket(m_Socket);
+	}
 };
